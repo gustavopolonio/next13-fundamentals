5a8cff189debe3aa2683d9a694a0d77361ee22a3
Gustavo <gustavopolonio1@gmail.com>
2024-04-29 17:50:08 -0300

    abstracting client component

---
+++ src/app/catalog/product/[...data]/add-to-cart-button.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useState } from 'react'
+
+export function AddToCartButton() {
+  const [count, setCount] = useState(0)
+
+  function addToCart() {
+    setCount((state) => state + 1)
+  }
+
+  return (
+    <button onClick={addToCart}>Adicionar produto ao carrinho ({count})</button>
+  )
+}
--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,10 +1,18 @@
+import { AddToCartButton } from './add-to-cart-button'
+
 interface ProductProps {
   params: {
     data: string[]
   }
 }
 
-export default function Product({ params }: ProductProps) {
+export default async function Product({ params }: ProductProps) {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const response = await fetch('https://api.github.com/users/gustavopolonio')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const user = await response.json()
+
   const [productId, size, color] = params.data
 
   return (
@@ -12,6 +20,8 @@ export default function Product({ params }: ProductProps) {
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+
+      <AddToCartButton />
     </div>
   )
 }
